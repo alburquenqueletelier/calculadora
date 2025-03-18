0d3a03f44a3eaba187712145e5bfb78fe6e1a24c
baal <alburquenque.letelier@gmail.com>
2025-03-18 16:24:19 -0300

    Setup calculadora

---
+++ calculadora.py
@@ -0,0 +1,24 @@
+def main():
+    input("Ingrese primer número: ", int)
+    input("Ingrese segundo número: ", int)
+
+    print("suma: ")
+    print("resta: ")
+    print("multiplicación: ")
+    print("división: ")
+
+
+def suma():
+    return
+
+def resta():
+    return
+
+def multiplicacion():
+    return
+
+def division():
+    return
+
+if __name__ == "__main__":
+    main()
